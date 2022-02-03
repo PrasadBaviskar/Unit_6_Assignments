eb280b207ff29f261bcb925897b8cd5e8825ba72
Prasad Baviskar <71400522+PrasadBaviskar@users.noreply.github.com>
2022-02-03 11:21:50 +0530

    Update index.js

--- Infinite_Scroll/index.js
+++ Infinite_Scroll/index.js
@@ -43,37 +43,4 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// let counter=1;
-// var parent=document.getElementById("main-div")
-// const getpost=(start,end)=>{
-//     try{
-//         console.log(start,end)
-//         for(let i=start;i<=end;i++){
-//             var ptag=document.createElement("p")
-//             ptag.innerHTML=`<h1>Masai Student ${i++}</h1>`;
-//             parent.append(ptag)
-//         }
 
-//     }catch(err){
-//         console.log(err);
-//     }
-// }
-// getpost(1,30)
-
-// function showData(){
-//     setTimeout(()=>{
-//       counter++;
-
-//       end=counter * 30;
-//       start=end-30;
-//         getpost(start,end)
-//        },300)
-// }
-
-// window.addEventListener('scroll',()=>{
-//     const {scrollHeight,scrollTop,clientHeight}=document.documentElement;
-//     if(scrollTop+clientHeight >= scrollHeight){
-//          console.log("I am Bottom")
-//          showData()
-//     }
-// })
